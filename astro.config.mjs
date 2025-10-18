eadf719dcc7faa37463669cd5147027e742a082b
suzza <suzza@susansilver.net>
2025-10-18 11:57:06 -0700

    configure and add local fonts

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,44 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import mdx from '@astrojs/mdx';
+import mdx from "@astrojs/mdx";
 
-import tailwindcss from '@tailwindcss/vite';
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://suzzastitches.com",
   integrations: [mdx()],
 
   vite: {
-    plugins: [tailwindcss()]
-  }
+    plugins: [tailwindcss()],
+  },
+  experimental: {
+    fonts: [
+      {
+        provider: "local",
+        name: "Solider",
+        cssVariable: "--font-solider",
+        variants: [
+          {
+            weight: 400,
+            style: "normal",
+            src: ["./src/assets/fonts/solider/Solide_Mirage-Mono_web.woff2"],
+          },
+        ],
+      },
+      {
+        provider: "local",
+        name: "Abordage",
+        cssVariable: "--font-abordage",
+        variants: [
+          {
+            weight: 400,
+            style: "normal",
+            src: ["./src/assets/fonts/abordage/Abordage-Regular.woff2"],
+          },
+        ],
+      },
+    ],
+  },
 });
